a5ea11fb362f4a65bc6fd8a30edd71d24e6e8920
lac <vitalyaal@mail.ru>
2020-04-14 17:01:52 +0300

    переопределен метод compare для сравнения BigDecimal

--- src/BigNumber/JavaBigDecimal.java
+++ src/BigNumber/JavaBigDecimal.java
@@ -10,22 +10,22 @@ public class JavaBigDecimal {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
-        String [] s = new String[n];
 
-        String newArray[] = new String[n];
+        String s[] = new String[n + 2]; // в задаче n+2
 
-        for(int i= 0;i < newArray.length; i++)
-        {
-            newArray[i] = s[i];
+        for (int i = 0; i < n; i++) {
+            s[i] = scan.nextLine();
         }
 
-        Arrays.sort(newArray, Collections.reverseOrder(new Comparator<String>() {
+        Arrays.sort(s, Collections.reverseOrder(new Comparator<String>() {
 
             @Override
             public int compare(String a1, String a2) {
-                BigDecimal a = new BigDecimal(a1);
-                BigDecimal b = new BigDecimal(a2);
-                return a.compareTo(b);
+                if (a1 != null && a2 != null) {
+                    BigDecimal a = new BigDecimal(a1);
+                    BigDecimal b = new BigDecimal(a2);
+                    return a.compareTo(b);
+                } else return 0;
             }
         }));
 
